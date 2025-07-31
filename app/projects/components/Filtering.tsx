93c889bc8d92614490c1e2b4d69e2b6b6e8b6ac9
Justin Ng <justinng@Justins-MacBook-Air.local>
2025-07-31 17:22:01 +0100

    added filtering to projects page

---
+++ app/projects/components/Filtering.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+import { SearchParams } from "@/types/route";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+interface FilteringProps {
+  years: number[];
+  locations: string[];
+}
+
+const Filtering = ({ years, locations }: FilteringProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(name, value);
+
+    router.push(pathname + "?" + params.toString());
+  };
+  return (
+    <div className="flex gap-4 mb-8">
+      <select
+        name="year"
+        className="border border-gray-300 rounded-md px-4 py-2"
+        onChange={handleChange}
+        defaultValue={searchParams.get(SearchParams.YEAR) as string}
+      >
+        <option value="">Filter by Year</option>
+        {years.map((year) => (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        ))}
+      </select>
+
+      <select
+        name="location"
+        className="border border-gray-300 rounded-md px-4 py-2"
+        onChange={handleChange}
+        defaultValue={searchParams.get(SearchParams.LOCATION) as string}
+      >
+        <option value="">Filter by Location</option>
+        {locations.map((location) => (
+          <option key={location} value={location}>
+            {location}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Filtering;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,8 +3,29 @@ import SectionSplit from "@/components/SectionSplit";
 import Banner from "@/modules/Banner";
 import Link from "next/link";
 import Project from "./components/Project";
+import Filtering from "./components/Filtering";
+import { SearchParams } from "@/types/route";
 
-export default function ProjectsPage() {
+export default function ProjectsPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const years = [...new Set(projects.map((project) => project.year))];
+  const locations = [...new Set(projects.map((project) => project.location))];
+
+  const filteredProjects = projects.filter((project) => {
+    const year = searchParams[SearchParams.YEAR];
+    const location = searchParams[SearchParams.LOCATION];
+
+    if (year && location) {
+      return project.year == Number(year) && project.location == location;
+    } else if (year) {
+      return project.year == Number(year);
+    } else if (location) {
+      return project.location == location;
+    }
+  });
   return (
     <>
       <Banner />
@@ -12,9 +33,10 @@ export default function ProjectsPage() {
       <SectionSplit>
         <Container>
           <h1 className="mb-4">Our Work</h1>
+          <Filtering years={years} locations={locations} />
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-12">
-            {projects.map((project) => (
+            {filteredProjects.map((project) => (
               <Link href={`/projects/${project.id}`} key={project.title}>
                 <Project {...project} />
               </Link>
@@ -51,7 +73,7 @@ const projects = [
   {
     id: 4,
     title: "Project 4",
-    year: 2023,
+    year: 2025,
     location: "Stevenage",
     images: ["/hero.jpg"],
   },
